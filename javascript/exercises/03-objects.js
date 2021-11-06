38986b5496dc382846a4daccbc2d7ba8a981df27
muhsen <muhsen@pdx.edu>
2021-11-06 16:56:25 -0700

    excercise 3 done

--- javascript/exercises/03-objects.js
+++ javascript/exercises/03-objects.js
@@ -1,22 +1,28 @@
 let result = {};
 
 const insertItem = (key, value) => {
-  // insertItem the property with key and value into result
-  return key;
+	// insertItem the property with key and value into result
+	result[key] = value;
 };
 
 const deleteItem = (key) => {
-  // remove the property with key from result
-  return key;
+	// remove the property with key from result
+	delete result[key];
 };
 
 const lookupItem = (key) => {
-  // return the value from result that is associated with key
-  return key;
+	// return the value from result that is associated with key
+	if (result.hasOwnProperty(key)) {
+		return result[key];
+	}
+
+	return 'Item does not exist';
 };
 
 const printItems = () => {
-  // return a string of the concatenated key in result, separated by commas
+	// return a string of the concatenated key in result, separated by commas
+
+	return Object.keys(result).join(', ');
 };
 
 insertItem('hello', 'world');
